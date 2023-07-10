626c1d8d5423b170be78d32b9aa7976d0e61db19
Hyrum <hyrum.coleman@gmail.com>
2023-07-09 21:48:27 -0400

    format and update direction enum

--- src/hardware/motor.rs
+++ src/hardware/motor.rs
@@ -1,4 +1,11 @@
-use arduino_hal::{port::{Pin, mode::{Output, PwmOutput}}, hal::port::{PD5, PD6, PD7, PB0, PD3}, simple_pwm::Timer0Pwm};
+use arduino_hal::{
+    hal::port::{PB0, PD3, PD5, PD6, PD7},
+    port::{
+        mode::{Output, PwmOutput},
+        Pin,
+    },
+    simple_pwm::Timer0Pwm,
+};
 
 pub struct Motor {
     pub right_speed_pin: Pin<PwmOutput<Timer0Pwm>, PD5>,
@@ -6,11 +13,25 @@ pub struct Motor {
     pub right_power_pin: Pin<Output, PD7>,
     pub left_power_pin: Pin<Output, PB0>,
     pub standby_pin: Pin<Output, PD3>,
+    pub last_direction: Direction,
 }
 
 impl Motor {
-    pub fn new(right_speed_pin: Pin<PwmOutput<Timer0Pwm>, PD5>, left_speed_pin: Pin<PwmOutput<Timer0Pwm>, PD6>, right_power_pin: Pin<Output, PD7>, left_power_pin: Pin<Output, PB0>, standby_pin: Pin<Output, PD3>) -> Self {
-        Self { right_speed_pin, left_speed_pin, right_power_pin, left_power_pin, standby_pin }
+    pub fn new(
+        right_speed_pin: Pin<PwmOutput<Timer0Pwm>, PD5>,
+        left_speed_pin: Pin<PwmOutput<Timer0Pwm>, PD6>,
+        right_power_pin: Pin<Output, PD7>,
+        left_power_pin: Pin<Output, PB0>,
+        standby_pin: Pin<Output, PD3>,
+    ) -> Self {
+        Self {
+            right_speed_pin,
+            left_speed_pin,
+            right_power_pin,
+            left_power_pin,
+            standby_pin,
+            last_direction: Direction::None,
+        }
     }
 
     pub fn enable(&mut self) {
@@ -20,16 +41,19 @@ impl Motor {
     }
 
     pub fn drive(&mut self, direction: Direction, speed: u8) {
+
+        if direction == self.last_direction {
+            return
+        }
+
         match direction {
             Direction::Forwards => self.drive_forwards(speed),
             Direction::Backwards => self.drive_backwards(speed),
             Direction::Left => self.drive_left(speed),
             Direction::Right => self.drive_right(speed),
-            Direction::ForwardsLeft => self.drive_forwards_left(speed),
-            Direction::ForwardsRight => self.drive_forwards_right(speed),
-            Direction::BackwardsLeft => self.drive_backwards_left(speed),
-            Direction::BackwardsRight => self.drive_backwards_right(speed),
+            Direction::None => self.stop(),
         }
+        self.last_direction = direction;
     }
 
     pub fn stop(&mut self) {
@@ -66,43 +90,13 @@ impl Motor {
         self.right_speed_pin.set_duty(speed);
         self.left_speed_pin.set_duty(speed);
     }
-
-    fn drive_forwards_left(&mut self, speed: u8) {
-        self.right_power_pin.set_high();
-        self.left_power_pin.set_low();
-        self.right_speed_pin.set_duty(speed);
-        self.left_speed_pin.set_duty(speed - 50);
-    }
-
-    fn drive_forwards_right(&mut self, speed: u8) {
-        self.right_power_pin.set_low();
-        self.left_power_pin.set_high();
-        self.right_speed_pin.set_duty(speed - 50);
-        self.left_speed_pin.set_duty(speed);
-    }
-
-    fn drive_backwards_left(&mut self, speed: u8) {
-        self.right_power_pin.set_low();
-        self.left_power_pin.set_high();
-        self.right_speed_pin.set_duty(speed);
-        self.left_speed_pin.set_duty(speed - 50);
-    }
-
-    fn drive_backwards_right(&mut self, speed: u8) {
-        self.right_power_pin.set_high();
-        self.left_power_pin.set_low();
-        self.right_speed_pin.set_duty(speed - 50);
-        self.left_speed_pin.set_duty(speed);
-    }
 }
 
+#[derive(PartialEq)]
 pub enum Direction {
     Forwards,
     Backwards,
     Left,
     Right,
-    ForwardsLeft,
-    ForwardsRight,
-    BackwardsLeft,
-    BackwardsRight,
+    None,
 }
